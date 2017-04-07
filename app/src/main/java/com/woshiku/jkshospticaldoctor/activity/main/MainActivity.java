3253a32d6997b832ee795ed1f9ae467b78a211ec
woshiku <849383208@qq.com>
2017-04-07 11:42:29 +0800

    woshiku revise second data

---
+++ app/src/main/java/com/woshiku/jkshospticaldoctor/activity/main/MainActivity.java
@@ -0,0 +1,17 @@
+package com.woshiku.jkshospticaldoctor.activity.main;
+
+import com.woshiku.jkshospticaldoctor.R;
+import com.woshiku.jkshospticaldoctor.activity.BaseActivity;
+
+public class MainActivity extends BaseActivity {
+
+    @Override
+    protected void initViews() {
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void swipeBackCallback() {
+
+    }
+}
